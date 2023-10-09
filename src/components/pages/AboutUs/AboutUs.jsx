9d229e0c80b0d0214cb2e5f4d5369489f57915c3
ashiq73 <ashiq.buet73@gmail.com>
2023-10-09 17:53:02 +0600

    Events page and responsive and all functionaly check done

--- src/components/pages/AboutUs/AboutUs.jsx
+++ src/components/pages/AboutUs/AboutUs.jsx
@@ -33,7 +33,10 @@ const AboutUs = () => {
               products, but summarizing your offerings on the About Us page is
               crucial to tie them in with brand values in your messaging.
             </p>
-            <Link to className="btn btn-primary">
+            <Link
+              to="/events"
+              className="text-white w-full mt-2 bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            >
               See Events
             </Link>
           </div>
